00f4df7240e74809b75f26593d3e7f0a2868b19e
Peter Seibel <peter@gigamonkeys.com>
2010-12-05 20:11:35 -0800

    Deleting some dead code.

--- html.lisp
+++ html.lisp
@@ -48,24 +48,12 @@ guess from the first H1 in sexps."
   (with-foo-output (out)
     (emit-html
      `(:html
-        ,(make-head (or title (guess-title sexps)) stylesheets scripts)
+        (:head
+         (:title ,(or title (guess-title sexps)))
+         (:meta :http-equiv "Content-Type" :content "text/html; charset=UTF-8")
+         ,@(loop for s in stylesheets collect `(:link :rel "stylesheet" :href ,s :type "text/css"))
+         ,@(loop for s in scripts collect `(:script :src ,s)))
         ,(funcall rewriter sexps)))))
-
-(defun make-head (title stylesheets scripts)
-  `(:head
-    (:title ,title)
-    (:meta :http-equiv "Content-Type" :content "text/html; charset=UTF-8")
-    ,@(loop for s in stylesheets collect `(:link :rel "stylesheet" :href ,s :type "text/css"))
-    ,@(loop for s in scripts collect `(:script :src ,s))))
-
-(defun rewrite-body (sexps pre-mapping post-mapping tag-mappings)
-  "Given `sexps' extract the link-defs and then process the remaining
-sexp first with the pre-mapping processors, then remap tags and
-rewrite links, and finally with the post-mapping processors."
-  (multiple-value-bind (sexps links) (extract-link-defs sexps)
-    (let ((pre (apply #'compose pre-mapping))
-          (post (apply #'compose post-mapping)))
-      (funcall post (rewrite-links (remap-tags (funcall pre sexps) tag-mappings) links)))))
 
 (defun guess-title (sexps)
   (let ((first-h1 (find :h1 sexps :key (lambda (x) (and (consp x) (first x))))))
@@ -99,10 +87,6 @@ rewrite links, and finally with the post-mapping processors."
         (keyword `(,mapper ,@(mapcar walker-fn content)))
         (cons `(,mapper ,@(mapcar walker-fn content)))
         (symbol (funcall mapper sexp))))))
-
-(defun fix-comments (sexp) (footnotes :comment sexp :number-format "Comment ~d"))
-
-(defun fix-notes (sexp) (footnotes :note sexp))
 
 (defun htmlize-links (sexps)
   (multiple-value-bind (sexps links) (extract-link-defs sexps)
